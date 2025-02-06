9b4c36c4fe1da09ff9eca9a68815e6539741ddad
SamyrArchila <samirarchila@colegioguanenta.edu.co>
2025-02-06 15:40:43 -0500

    png

---
+++ minicalculadora_ guanentina.py
@@ -0,0 +1,11 @@
+import math
+print("---------------------------")
+print("1) suma")
+print("2) resta")
+print("3) multiplicacion")
+print("4) division")
+print("5) potencia")
+print("6) logaritmo")
+print("----------------------------")
+
+opc = 
